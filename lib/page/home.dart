0860c19f571a247e2515ce88de7a2f7e5d70ec8c
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-03-13 10:41:12 +0700

    refactor HomePage pakai MainLayout dan update navigasi ke OrderPage

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import '../page/order_page.dart';
+import '../layout/main_layout.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: Center(
+    return MainLayout(
+      title: "Home",
+      child: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pushNamed(context, '/order');
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const OrderPage(),
+              ),
+            );
           },
-          child: const Text("Order Food"),
+          child: const Text("Go To Order"),
         ),
       ),
     );
