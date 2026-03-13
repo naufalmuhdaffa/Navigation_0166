43338a4b85bc6f94d989252e7d4d8842692beb5e
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-03-13 10:06:09 +0700

    menambahkan widget, tombol login, dan navigasi ke halaman home

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -5,6 +5,15 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/home');
+          },
+          child: const Text("Login"),
+        ),
+      ),
+    );
   }
 }
