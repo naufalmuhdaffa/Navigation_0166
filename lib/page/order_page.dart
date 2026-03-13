2c17215ecb44d1edcc849ebd275b4ec5a1fa8569
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-03-13 10:41:33 +0700

    ubah OrderPage menjadi StatefulWidget dan tambahkan input untuk makanan dan minuman

--- lib/page/order_page.dart
+++ lib/page/order_page.dart
@@ -1,30 +1,65 @@
 import 'package:flutter/material.dart';
 import 'order_detail_page.dart';
 
-class OrderPage extends StatelessWidget {
+class OrderPage extends StatefulWidget {
   const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+
+  int makananQty = 0;
+  int minumanQty = 0;
+
+  int totalHarga = 0;
+
+  void hitungTotal() {
+    totalHarga = (makananQty * 20000) + (minumanQty * 5000);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Order Food")),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const OrderDetailPage(
-                  makanan: "Nasi Goreng",
-                  minuman: "Es Teh",
-                  jumlahMakanan: "2",
-                  jumlahMinuman: "1",
-                  totalHarga: 30000,
-                ),
-              ),
-            );
-          },
-          child: const Text("Checkout"),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: makananController,
+              decoration: const InputDecoration(labelText: "Food"),
+            ),
+            TextField(
+              controller: minumanController,
+              decoration: const InputDecoration(labelText: "Drink"),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                hitungTotal();
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => OrderDetailPage(
+                      makanan: makananController.text,
+                      minuman: minumanController.text,
+                      jumlahMakanan: makananQty.toString(),
+                      jumlahMinuman: minumanQty.toString(),
+                      totalHarga: totalHarga,
+                    ),
+                  ),
+                );
+              },
+              child: const Text("Checkout"),
+            ),
+          ],
         ),
       ),
     );
