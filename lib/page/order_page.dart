ffa1b5f1e2789ce59c18b8fcf0a45a365187b647
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-03-13 10:17:37 +0700

    menambahkan widget

--- lib/page/order_page.dart
+++ lib/page/order_page.dart
@@ -5,6 +5,8 @@ class OrderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Order Food")),
+    );
   }
 }
