006576b7caae7f588fcbfc61bf650a413b80889a
naufalmuhdaffa <naufaldaffa878@gmail.com>
2026-03-13 10:21:47 +0700

    menambahkan variabel, properti, dan menambahkan widget

--- lib/page/order_detail_page.dart
+++ lib/page/order_detail_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 class OrderDetailPage extends StatelessWidget {
-  const OrderDetailPage({super.key});
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+
+  const OrderDetailPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Order Detail")),
+    );
   }
 }
